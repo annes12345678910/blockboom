0f22c371bb5c6b12d4b5203a09e062a1506caabe
annes12345678910 <anneswidow@gmail.com>
2025-07-09 20:27:59 +0300

    Create blocks.js

---
+++ blocks.js
@@ -0,0 +1,19 @@
+Blockly.defineBlocksWithJsonArray([
+  {
+    type: "event_start",
+    message0: "when green flag clicked",
+    nextStatement: null,
+    colour: 120
+  },
+  {
+    type: "move",
+    message0: "move %1 x and %2 y",
+    args0: [
+      { type: "field_number", name: "X", value: 0 },
+      { type: "field_number", name: "Y", value: 0 }
+    ],
+    previousStatement: null,
+    nextStatement: null,
+    colour: 230
+  }
+]);
